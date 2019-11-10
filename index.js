f5621175c10af5ed165aa73832edfeb98a36000d
FrancisGichuhi <francisgichh@gmail.com>
2019-11-09 22:38:37 -0500

    team ID and ABR

--- index.js
+++ index.js
@@ -5,14 +5,27 @@ const teams = require('./teams.json')
 
 app.get('/teams', (request, response) => {
     response.send(teams)
-  })
-
-app.get('/teams/:id/', (request, response) => {
-  let teamById = teams.filter( (team) => {
-    return team.id == request.params.id
-  })
-    response.send(teamById)
 })
+
+app.get('/teams/:teamID/', (request, response) => {
+    let teamRequested = teams.filter( (item) => {
+    return item.teamID == request.params.teamID
+  })
+    response.send(teamRequested)
+})
+
+app.get('/teams/:teamID/:abbreviation/', (request, response) => {
+    let teamByAbbreviation = teams.filter( (team) => {
+    return team.abbreviation == request.params.abbreviation
+  })  
+    response.send(teamByAbbreviation)
+}) 
+
+app.all('*', (request, response) => {
+    console.log({request})
+    response.send('Oops, please enter correct url!')
+}) 
+
 
 app.listen(1337, () => {
     console.log('localhost:1337')
