2862cee33efe0eb50d3054c39e6b0a6efc3021ef
FrancisGichuhi <francisgichh@gmail.com>
2019-11-13 15:35:02 -0500

    post teams new update

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 
-const express = require ('express')
+const express = require ('express') 
 const app = express ()
 const teams = require('./teams.json')
+const http_port = 1337
 const bodyParser = require('body-parser')
 
-app.get('/teams', (request, response) => {
+
+app.get('/teams', (_request, response) => {
     response.send(teams)
 })
 
-app.get('*/teams/:idFilter', (request, response) => {
-    let specificTeam = teams.filter( (team) => {
-        return team.id == request.params.idFilter
+app.get('/teams/:filter', (request, response) => {
+    let filter = request.params.filter
+    let result = teams.filter( (team) => {
+    return team.id == filter || team.abbreviation == filter || team.division == filter || team.conference == filter
   })
-    response.send(specificTeam)
+    let locations = result.map((team) => {
+        return team.location
+    })
+    response.send(locations)
+    console.log(filter)
 })
 
-app.get('/teams/:idFilter/:abbreviation', (request, response) => {
-    let teamByAbbreviation = teams.filter( (team) => {
-        return team.abbreviation == request.params.abbreviation
-  })  
-    response.send(teamByAbbreviation)
-}) 
+// app.get('/teams/:idFilter/:abbreviation', (request, response) => {
+//     let teamByAbbreviation = teams.filter( (team) => {
+//         return team.abbreviation == request.params.abbreviation
+//   })  
+//     response.send(teamByAbbreviation)
+// }) 
 
 app.post('/teams', bodyParser.json(), (request, response) => {
-    const body = request.body || {}
+    const body = request.body
+    let newTeams = teams.concat(body)
+    if (!"id" || !"location" || !"mascot" || !"abbreviation" || !"conference" || !"division") 
+    if (!body.id || !body.location || !body.mascot || !body.abbreviation || !body.conference || !body.division) {
+        response.status(400).send('The following attributes are required: location, mascot, abbreviation, conference, division')
+    }
     console.log({body})
-    response.send(body)
+    response.send(newTeams)
 })
 
 app.all('*', (request, response) => {
