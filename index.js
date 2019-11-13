208066815f3f89c93c80777e288a7826972ff1af
FrancisGichuhi <francisgichh@gmail.com>
2019-11-13 00:55:35 -0500

    POSTHandler(Will create a new team)

--- index.js
+++ index.js
@@ -2,24 +2,31 @@
 const express = require ('express')
 const app = express ()
 const teams = require('./teams.json')
+const bodyParser = require('body-parser')
 
 app.get('/teams', (request, response) => {
     response.send(teams)
 })
 
-app.get('/teams/:teamID/', (request, response) => {
-    let teamRequested = teams.filter( (item) => {
-    return item.teamID == request.params.teamID
+app.get('*/teams/:idFilter', (request, response) => {
+    let specificTeam = teams.filter( (team) => {
+        return team.id == request.params.idFilter
   })
-    response.send(teamRequested)
+    response.send(specificTeam)
 })
 
-app.get('/teams/:teamID/:abbreviation/', (request, response) => {
+app.get('/teams/:idFilter/:abbreviation', (request, response) => {
     let teamByAbbreviation = teams.filter( (team) => {
-    return team.abbreviation == request.params.abbreviation
+        return team.abbreviation == request.params.abbreviation
   })  
     response.send(teamByAbbreviation)
 }) 
+
+app.post('/teams', bodyParser.json(), (request, response) => {
+    const body = request.body || {}
+    console.log({body})
+    response.send(body)
+})
 
 app.all('*', (request, response) => {
     console.log({request})
@@ -31,3 +38,5 @@ app.listen(1337, () => {
     console.log('localhost:1337')
 
 })
+
+module.exports = app
